6840959c8cc3031f4266d97a9072291375804bce
Shashank Aryan <aryanshashank31@gmail.com>
2017-12-19 21:04:02 +0530

    Tests added for all the components.

--- js/App.jsx
+++ js/App.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import React, { Component } from 'react'
+import { WebSocket } from 'mock-socket'
 import Table from './Table'
 import Loading from './Loading'
 import Indicator from './Indicators'
